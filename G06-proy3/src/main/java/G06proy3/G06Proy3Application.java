f7dd11720453b03661974d2fc03c806f54fba420
vomisa29 <p.mendezm@uniandes.edu.co>
2023-11-24 15:53:13 -0500

    commit base

---
+++ G06-proy3/src/main/java/G06proy3/G06Proy3Application.java
@@ -0,0 +1,13 @@
+package G06proy3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G06Proy3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G06Proy3Application.class, args);
+	}
+
+}
---
+++ G06-proy3/src/test/java/G06proy3/G06Proy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package G06proy3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G06Proy3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
